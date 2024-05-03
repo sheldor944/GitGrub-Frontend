c3fd0dbdb4afb4a3d1d924deee84a404753e7ddc
Aniket Joarder <joarderaniket@gmail.com>
2024-05-04 04:32:27 +0600

    something happenedn wrong very wrong

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,12 +1,18 @@
+import { useGetInventory } from "@/api/InventoryApi";
 import {
+  useAddInventory,
   useCreateMyRestaurant,
   useGetMyRestaurant,
   useGetMyRestaurantOrders,
   useUpdateMyRestaurant,
 } from "@/api/MyRestaurantApi";
+import InventoryTable from "@/components/InventoryTable";
 import OrderItemCard from "@/components/OrderItemCard";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import ManageInvetory from "@/forms/manage-restaurant-form/ManageInvetory";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
+
+
 
 const ManageRestaurantPage = () => {
   const { createRestaurant, isLoading: isCreateLoading } =
@@ -14,17 +20,21 @@ const ManageRestaurantPage = () => {
   const { restaurant } = useGetMyRestaurant();
   const { updateRestaurant, isLoading: isUpdateLoading } =
     useUpdateMyRestaurant();
-
   const { orders } = useGetMyRestaurantOrders();
+  const {addInventory, isLoading } = useAddInventory();
 
   const isEditing = !!restaurant;
 
   return (
-    <Tabs defaultValue="orders">
-      <TabsList>
+    <Tabs defaultValue="orders"  >
+      <div className="bg-gray-50 rounded-lg">
+      <TabsList className="grid-container h-auto px-2">
         <TabsTrigger value="orders">Orders</TabsTrigger>
         <TabsTrigger value="manage-restaurant">Manage Restaurant</TabsTrigger>
+        <TabsTrigger value="manage-inventory">Manage Inventory</TabsTrigger>
+        
       </TabsList>
+      </div>
       <TabsContent
         value="orders"
         className="space-y-5 bg-gray-50 p-10 rounded-lg"
@@ -40,6 +50,18 @@ const ManageRestaurantPage = () => {
           onSave={isEditing ? updateRestaurant : createRestaurant}
           isLoading={isCreateLoading || isUpdateLoading}
         />
+      </TabsContent>
+      <TabsContent value="manage-inventory" className="space-y-5 bg-gray-50 p-10 rounded-lg">
+         <ManageInvetory
+          onAdd={addInventory}
+          isLoading={isLoading}
+          buttonText="Add Item"
+          >
+          
+         </ManageInvetory>
+         <InventoryTable>
+
+         </InventoryTable>
       </TabsContent>
     </Tabs>
   );
