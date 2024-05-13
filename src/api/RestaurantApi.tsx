c7d8362651d83e0ae97011a1e6dca21435e513a9
sheldor944 <mehrajul.abedin.944@gmail.com>
2024-05-14 02:49:52 +0600

    search parameter fixed

--- src/api/RestaurantApi.tsx
+++ src/api/RestaurantApi.tsx
@@ -35,15 +35,13 @@ export const useSearchRestaurants = (
 ) => {
   const createSearchRequest = async (): Promise<RestaurantSearchResponse> => {
     const params = new URLSearchParams();
-    
-    console.log(city)
     params.set("searchQuery", searchState.searchQuery);
     params.set("page", searchState.page.toString());
     params.set("selectedCuisines", searchState.selectedCuisines.join(","));
     params.set("sortOption", searchState.sortOption);
 
     const response = await fetch(
-      `${API_BASE_URL}/api/restaurant/search/${city}`
+      `${API_BASE_URL}/api/restaurant/search/${city}?${params.toString()}`
     );
     
 
